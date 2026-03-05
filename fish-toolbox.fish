1daffa5047304df6d3c94146a448b185f6f2063c
DynamicCast03 <wdynamica@gmail.com>
2026-03-05 13:36:15 +0800

    remove root special prompt

--- fish-toolbox.fish
+++ fish-toolbox.fish
@@ -7,23 +7,16 @@ function fish_prompt --description 'Informative prompt'
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
 
-    if functions -q fish_is_root_user; and fish_is_root_user
-        printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
-                                                                 and set_color $fish_color_cwd_root
-                                                                 or set_color $fish_color_cwd) \
-                    (prompt_pwd) (set_color normal)
-    else
-        set -l status_color (set_color $fish_color_status)
-        set -l statusb_color (set_color --bold $fish_color_status)
-        set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color --bold $fish_color_status)
+    set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-        set -l conda_info ""
-        if set -q CONDA_DEFAULT_ENV
-            set conda_info (set_color yellow)"("$CONDA_DEFAULT_ENV") " (set_color normal)
-        end
-
-        printf '[%s] %s%s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") "$conda_info" (set_color brblue) \
-                    $USER (prompt_hostname) (set_color $fish_color_cwd) (string replace -r '^'"$HOME" '~' $PWD) $pipestatus_string \
-                    (set_color normal)
+    set -l conda_info ""
+    if set -q CONDA_DEFAULT_ENV
+        set conda_info (set_color yellow)"("$CONDA_DEFAULT_ENV") " (set_color normal)
     end
+
+    printf '[%s] %s%s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") "$conda_info" (set_color brblue) \
+                $USER (prompt_hostname) (set_color $fish_color_cwd) (string replace -r '^'"$HOME" '~' $PWD) $pipestatus_string \
+                (set_color normal)
 end
